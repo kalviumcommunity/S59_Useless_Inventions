fa73f1257f5b1843dfacc1f73153e1bf53a4e287
ChiragSareen7 <chiragsareen8054@gmail.com>
2024-03-07 17:24:47 +0530

    front-end

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Front from './Components/Front'
+
+function App() {
+
+
+  return (
+    <div>
+    <Front />
+    </div>
+  )
+}
+
+export default App
